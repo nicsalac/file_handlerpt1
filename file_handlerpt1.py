b35f86844028171afbb761c9f4db650ab95aa81c
nicsalac <nicoletteirishsalac127@gmail.com>
2023-04-29 15:58:48 +0800

    open file

--- file_handlerpt1.py
+++ file_handlerpt1.py
@@ -1,1 +1,14 @@
 # problem 4- file handling
+
+
+def process():
+    # open integers.txt (read), double.txt(append), triple.txt (append)
+    with open("integers.txt") as input_file, open("double.txt", "a") as output_even_file, open("triple.txt", "a"): 
+    # read integers.txt 
+    # if even integers
+    #get the square
+    # write to double.txt
+    #else
+    # if odd integers
+    #  get the cube 
+    # write to triple.txt
