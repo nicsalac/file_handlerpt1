139fdd0e5e04bb3ba780bd5a40d0b39ecb4aef35
nicsalac <nicoletteirishsalac127@gmail.com>
2023-04-29 16:12:40 +0800

    squaring the even integers

--- file_handlerpt1.py
+++ file_handlerpt1.py
@@ -3,10 +3,15 @@
 
 def process():
     # open integers.txt (read), double.txt(append), triple.txt (append)
-    with open("integers.txt") as input_file, open("double.txt", "a") as output_even_file, open("triple.txt", "a"): 
-    # read integers.txt 
-    # if even integers
-    #get the square
+    with open("integers.txt") as input_file, open("double.txt", "a") as output_even_file, open("triple.txt", "a") as output_odd_file: 
+    # read integers.txt    
+        for line in input_file:
+            input_num = int(line)
+            # if even integers
+            if input_num % 2 == 0:
+                #get the square
+                square = input_num * input_num
+
     # write to double.txt
     #else
     # if odd integers
